57a25d07a35f4e1b45f54949932bc0d176c6c40f
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-04-06 01:04:53 +0800

    add more slides!

--- javascript/game.js
+++ javascript/game.js
@@ -1,8 +1,29 @@
-function startGame() {
-	$('section[data-page="2"]').fadeIn(pageFadeTime);
-	var currentPage = 2;
+var currentPage = 1;
 
-	var interval = setInterval(function() {
+function startGame() {
+	$(`section[data-page="${currentPage}"]`).fadeIn(pageFadeTime);
+	play();
+
+	$('.button[data-event="time-passed"]').click(function() {
+		const numberOfPages = currentPage;
+		const guess = $('input[data-event="time-passed"]').val();
+
+		if (checkTimePassed(guess, numberOfPages)) {
+			$(`section[data-page="${currentPage + 1}"] .wrong`).hide();
+		} else {
+			$(`section[data-page="${currentPage + 1}"] .right`).hide();
+		}
+		play();
+	});
+}
+
+function checkTimePassed(timeGuess, numberOfPages) {
+	const actual = (startFadeOut + (numberOfPages - 1) * slideTime + pageFadeTime) / 1000;
+	return Math.abs(parseInt(timeGuess) - actual) <= 0.5;
+}
+
+function play() {
+	const interval = setInterval(function() {
 		$(`section[data-page="${currentPage}"]`).fadeOut(pageFadeTime, function() {
 			currentPage++;
 			$(`section[data-page="${currentPage}"]`).fadeIn(pageFadeTime);
--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('section').hide();
-	$('section[data-page="1"]').show();
+	$('section[data-page="0"]').show();
 
 	$('.start-button').click(function(e) {
 		$(e.target).parents('section').fadeOut(startFadeOut, function() {
